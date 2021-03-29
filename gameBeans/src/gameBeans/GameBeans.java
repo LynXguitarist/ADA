9120a03feddec238df2902f2f0991c512b4e361b
frederico_lopes <fmc.lopes@campus.fct.unl.pt>
2021-03-29 13:05:28 +0100

    pieton part

--- gameBeans/src/gameBeans/GameBeans.java
+++ gameBeans/src/gameBeans/GameBeans.java
@@ -1,16 +1,13 @@
 package gameBeans;
 
-import java.util.LinkedList;
 import java.util.List;
 
 public class GameBeans {
 
-	private static final int SMALLEST_SIZE = -100; // its said in the description that -100 is the smallest size
-
 	private List<Integer> piles; // List of piles
 
-	private int depth, result, pilesLeft;
-	private short currentPlayer; // 0->Jaba, 1->Pieton
+	private int depth, result, pilesLeft; // result = Jaba's points
+	private short currentPlayer; // 0-> Jaba, 1-> Pieton
 
 	public GameBeans(int depth, List<Integer> piles, int pilesLength, short currentPlayer) {
 		this.depth = depth;
@@ -42,16 +39,77 @@ public class GameBeans {
 
 	// Options:
 	// - First element
-	// - First to First + depth options(0 -> 0 + depth)
+	// - First element + depth options(0 -> 0 + depth)
 	// - Last element
 	// - Last element + depth options(last -> last - depth)
 	private void pietonPlays() {
-		int max = SMALLEST_SIZE;
-		List<Integer> currentOption = new LinkedList<>();
-		
-		// Has depth * 2 options
-		for(int i = 0; i < depth * 2; i++) {
-			
+		boolean isFromStart = true;
+
+		int max = piles.get(0); // first element
+		int length = 1; // amount of piles to remove
+		int lastIndex = piles.size() - 1;
+
+		if (depth == 1) {
+			if (max > piles.get(lastIndex)) // compares first with last element
+				piles.remove(0);
+			else
+				piles.remove(lastIndex);
+		}
+		// from first
+		int lastSum = piles.get(0); // starts has first element
+		int current = piles.get(1); // starts has second element
+		int sum = lastSum + current;
+
+		if (sum > max) { // check first with second
+			max = sum;
+			length = 2;
+		}
+		// from last
+		int lastSumL = piles.get(lastIndex); // starts has last element
+		int currentL = piles.get(lastIndex - 1); // starts has penultimate element
+		int sumL = lastSumL + currentL;
+
+		if (lastSumL > max) { // check last
+			max = lastSumL;
+			length = 1;
+			isFromStart = false;
+		}
+		if (sumL > max) { // check last with penultimate
+			max = sumL;
+			length = 2;
+			isFromStart = false;
+		}
+
+		for (int i = 2; i < depth; i++) {
+			// from first
+			lastSum = sum;
+			current = piles.get(i);
+			sum = lastSum + current;
+			if (sum > max) {
+				max = sum;
+				length = i + 1;
+				isFromStart = true;
+			}
+			// from last
+			lastSumL = sumL;
+			currentL = piles.get(lastIndex - i);
+			sumL = lastSumL + currentL;
+			if (sumL > max) {
+				max = sumL;
+				length = i + 1;
+				isFromStart = false;
+			}
+		}
+		removePiles(isFromStart, length);
+	}
+
+	private void removePiles(boolean isFromStart, int length) {
+
+		for (int i = 0; i < length; i++) {
+			if (isFromStart)
+				piles.remove(0);
+			else
+				piles.remove(piles.size() - 1);
 		}
 	}
 
